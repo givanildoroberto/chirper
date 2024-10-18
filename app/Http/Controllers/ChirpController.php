d117d8acf8729a43b7aa00d375cbb31904720801
givanildoroberto <givanildoroberto@gmail.com>
2024-10-18 17:56:47 -0300

    index page done

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -1,19 +1,19 @@
 <?php
-
+ 
 namespace App\Http\Controllers;
-
+ 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response
-    {
+     public function index(): Response 
+     {
         //
         return response('Hello, World!');
         return Inertia::render('Chirps/Index', [ ]);
