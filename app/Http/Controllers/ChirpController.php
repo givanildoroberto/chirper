1a684c49d0c7bb17e99ee9d4fc8d5f982b6a5220
givanildoroberto <givanildoroberto@gmail.com>
2024-10-25 18:34:52 -0300

    showing chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -15,9 +15,8 @@ class ChirpController extends Controller
      */
      public function index(): Response 
      {
-
         return Inertia::render('Chirps/Index', ['chirps' => Chirp::with('user:id,name')->latest()->get(), ]);
-    }
+     }
 
     /**
      * Show the form for creating a new resource.
