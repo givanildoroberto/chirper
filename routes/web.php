232f2a4ac80872d7101dc44444269f07a09380ed
givanildoroberto <givanildoroberto@gmail.com>
2024-10-29 11:51:51 -0300

    Editing Chirps

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,6 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::resource('chirps', ChirpController::class)
-    ->only(['index', 'store'])
     ->only(['index', 'store', 'update'])
     ->middleware(['auth', 'verified']);
 
